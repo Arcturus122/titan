9e1bc3e1eee4bde512397fdeacd1d69219ed9667
Louis-Alexis Dubief <louisalexis.dubief@gmail.com>
2023-03-29 23:29:30 +0200

    fixup! adding workflow

--- run.sh
+++ run.sh
@@ -6,5 +6,4 @@ python manage.py migrate --no-input
 gunicorn titan.wsgi:application --bind 0.0.0.0:8000 &
 
 unlink /etc/nginx/sites-enabled/default
-
-nginx -g "daemon off;"
+nginx -g 'daemon off;'
