b98f272d3c6e16b60cae347392abd3d6368d7150
Louisalexis.Dubief <louisalexis.dubief@trafigura.com>
2023-03-30 13:37:34 +0200

    fixup! adding workflow

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+pwd
+ls -la
+
 python manage.py makemigrations
 python manage.py migrate --no-input
 
 gunicorn titan.wsgi:application --bind 0.0.0.0:8000 &
 
 unlink /etc/nginx/sites-enabled/default
+
 nginx -g 'daemon off;'
