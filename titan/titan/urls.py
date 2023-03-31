21f722ace16754cc21384a3af384034c4fa969c9
Louisalexis.Dubief <louisalexis.dubief@trafigura.com>
2023-03-31 11:44:21 +0200

    fixup! updating urls

--- titan/titan/urls.py
+++ titan/titan/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 from backend.views import index
+from django.conf.urls import  url
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
-    path(r'^graphql', csrf_exempt(GraphQLView.as_view(graphiql=True)))
+    url(r'^graphql', csrf_exempt(GraphQLView.as_view(graphiql=True)))
 ]
