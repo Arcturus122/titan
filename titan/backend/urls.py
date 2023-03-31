5b671819df6ff4f20adbcf22aeb44d4038cd5d33
Louisalexis.Dubief <louisalexis.dubief@trafigura.com>
2023-03-31 11:22:47 +0200

    updating / route

--- titan/backend/urls.py
+++ titan/backend/urls.py
@@ -1,8 +1,1 @@
 from django.urls import path
-
-from . import views
-
-app_name = 'backend'
-urlpatterns = [
-    path('', views.index, name='index')
-]
--- titan/titan/urls.py
+++ titan/titan/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from backend.views import index
 
 urlpatterns = [
-    path('/', include('backend.urls')),
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True)))
 ]
